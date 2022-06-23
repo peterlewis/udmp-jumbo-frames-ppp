983bbde6948caf6004c7eb8c1df8fa482576d9b9
TotalGriffLock <john@theshado.ws>
2022-06-23 22:14:21 +0100

    Update 11-change-ppp-mtu.sh

--- 11-change-ppp-mtu.sh
+++ 11-change-ppp-mtu.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Which PPP interfaces to monitor?
+# You can list multiple interfaces like this
+# MINTERFACES="^(ppp0|ppp1|ppp10)$"
+MINTERFACES="^(ppp0)$"
 # Desired MTU of PPP interfaces
 PTARGET=1500
 function check_mtu {
@@ -12,14 +16,19 @@ while true; do
     pinterfaces=$(ls /etc/ppp/peers/)
     for pinterface in $pinterfaces
     do
-      sed -i 's/ '$(($PTARGET-8))'/ '$PTARGET'/g' /etc/ppp/peers/$pinterface
-      einterface=$(grep 'plugin rp-pppoe.so' /etc/ppp/peers/$pinterface | sed 's/plugin rp-pppoe.so //')
-      ip link set dev $einterface mtu $(($PTARGET+8))
-      # Maybe your PPPoE is over a VLAN and you need this instead
-      # ip link set dev $einterface mtu $(($PTARGET+12)) && ip link set dev $einterface.6 mtu $(($PTARGET+8))
-      ifconfig $einterface down && ifconfig $einterface up
+      if [[ $pinterfaces =~ $MINTERFACES ]]; then
+        changes=true
+        sed -i 's/ '$(($PTARGET-8))'/ '$PTARGET'/g' /etc/ppp/peers/$pinterface
+        einterface=$(grep 'plugin rp-pppoe.so' /etc/ppp/peers/$pinterface | sed 's/plugin rp-pppoe.so //')
+        ip link set dev $einterface mtu $(($PTARGET+8))
+        # Maybe your PPPoE is over a VLAN and you need this instead
+        # ip link set dev $einterface mtu $(($PTARGET+12)) && ip link set dev $einterface.6 mtu $(($PTARGET+8))
+        ifconfig $einterface down && ifconfig $einterface up
+      fi
     done
-    killall pppd
+    if [[ $changes == "true" ]]; then
+      killall pppd
+    fi
   fi
   sleep 5
 done
