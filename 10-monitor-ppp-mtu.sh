8901012b3dd3aa62dc4ef4b3b9cbc78ed8b1e166
TotalGriffLock <john@theshado.ws>
2022-10-09 11:02:32 +0100

    Update 10-monitor-ppp-mtu.sh
    Detect the appropriate shell interpreter to run the script in

--- 10-monitor-ppp-mtu.sh
+++ 10-monitor-ppp-mtu.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 # Run our monitoring script on startup
-bash /mnt/data/11-change-ppp-mtu.sh &
+# /bin/sh is different on the UDM-Pro and the UDM-SE
+# On the UDM-Pro it is the busybox shell interpreter
+# On the UDM-SE it is dash BUT full bash is also available
+# We want to run in busybox shell on a UDM-Pro and bash on a UDM-SE
+
+if [ -f /bin/bash ]; then
+  /bin/bash /mnt/data/11-change-ppp-mtu.sh &
+else
+  /bin/sh /mnt/data/11-change-ppp-mtu.sh &
+fi
